a5e0d06ed024c386875eb90b1cf310e380ce1c0a
Anastasia Surikova <surikana@fel.cvut.cz>
2017-04-07 12:20:07 +0200

    AndroidApp part (Luky)

---
+++ src/main/java/cz/cvut/fel/coursework/androidapp/Start.java
@@ -0,0 +1,10 @@
+package cz.cvut.fel.coursework.androidapp;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        // Luky, put your stuff in cz.cvut.fel.coursework.androidapp
+
+    }
+}
