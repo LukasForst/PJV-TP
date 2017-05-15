d1d093c7e10e4645eadd9b8b5b3184477d9f1c30
Anastasia Surikova <surikana@fel.cvut.cz>
2017-05-15 19:13:00 +0200

    ip tests

--- src/main/java/cz/cvut/fel/coursework/SERVICES/IPIdentifier.java
+++ src/main/java/cz/cvut/fel/coursework/SERVICES/IPIdentifier.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Enumeration;
 
 /**
@@ -14,6 +15,9 @@ import java.util.Enumeration;
 
 public class IPIdentifier {
 
+    private static ArrayList<String> ip_types = new ArrayList<String>();
+    private static ArrayList<String> value = new ArrayList<String>();
+
     /**
      * Gets IPv4 address of machine.
      * @return String IPv4 address
@@ -21,20 +25,45 @@ public class IPIdentifier {
 
     public String getIP(){
 
-        String finalIP;
-
-        InetAddress detectedIP = null;
-
         try {
-            detectedIP = InetAddress.getLocalHost();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
+            String ip;
+            int i = 0;
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface iface = interfaces.nextElement();
+                if (iface.isLoopback() || !iface.isUp()) continue;
+                Enumeration<InetAddress> addresses = iface.getInetAddresses();
+                while(addresses.hasMoreElements()) {
+                    InetAddress addr = addresses.nextElement();
+                    ip = addr.getHostAddress();
+                    ip = ip.replace("%" + iface.getDisplayName(), "");
+                    ip_types.add(iface.getDisplayName());
+                    value.add(ip);
+                    i++;
+                }
+            }
+            for (int j = 0; j < ip_types.size(); j++) {
+                System.out.println(ip_types.get(j) + " " + value.get(j));
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
         }
+        return "";
 
-        finalIP = detectedIP.toString();
-        finalIP = finalIP.substring(finalIP.lastIndexOf("/") + 1);
-
-        return finalIP;
+//        String finalIP;
+//
+//        InetAddress detectedIP = null;
+//
+//        try {
+//            detectedIP = InetAddress.getByAddress();
+//        } catch (UnknownHostException e) {
+//            e.printStackTrace();
+//        }
+//
+//        finalIP = detectedIP.toString();
+//        finalIP = finalIP.substring(finalIP.lastIndexOf("/") + 1);
+//
+//        return finalIP;
 
     }
 }
