1f22f50cc0c27be93c74893cca6224156413d9ea
Anastasia Surikova <surikana@fel.cvut.cz>
2017-05-15 19:33:04 +0200

    finally correct ip (i hope)

--- src/main/java/cz/cvut/fel/coursework/SERVICES/IPIdentifier.java
+++ src/main/java/cz/cvut/fel/coursework/SERVICES/IPIdentifier.java
@@ -13,9 +13,6 @@ import java.util.Enumeration;
 
 public class IPIdentifier {
 
-    private static ArrayList<String> ip_types = new ArrayList<String>();
-    private static ArrayList<String> value = new ArrayList<String>();
-
     /**
      * Gets IPv4 address of machine.
      * @return String IPv4 address
@@ -25,54 +22,23 @@ public class IPIdentifier {
 
         try {
             String ip;
-            int i = 0;
-            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-            while (interfaces.hasMoreElements()) {
-                NetworkInterface iface = interfaces.nextElement();
-                if (iface.isLoopback() || !iface.isUp()) continue;
-                Enumeration<InetAddress> addresses = iface.getInetAddresses();
-                while(addresses.hasMoreElements()) {
-                    InetAddress addr = addresses.nextElement();
-                    ip = addr.getHostAddress();
-                    ip = ip.replace("%" + iface.getDisplayName(), "");
-                    ip_types.add(iface.getDisplayName());
-                    value.add(ip);
-                    i++;
-                }
-            }
+
             for (NetworkInterface ni :
                     Collections.list(NetworkInterface.getNetworkInterfaces())) {
                 for (InetAddress address : Collections.list(ni.getInetAddresses())) {
                     if (address instanceof Inet4Address) {
-                        System.out.println(address);
+                        ip = address.toString();
+                        if(!ip.startsWith("127.")) {
+                            return ip.substring(ip.lastIndexOf("/") + 1);
+                        }
                     }
                 }
             }
-//            for (int j = 0; j < ip_types.size(); j++) {
-//                if (value.get(j) instanceof Inet4Address) {
-//                    System.out.println(value.get(j));
-//                }
-//                System.out.println(ip_types.get(j) + " " + value.get(j));
-//            }
+
         } catch (SocketException e) {
             throw new RuntimeException(e);
         }
         return "";
-
-//        String finalIP;
-//
-//        InetAddress detectedIP = null;
-//
-//        try {
-//            detectedIP = InetAddress.getByAddress();
-//        } catch (UnknownHostException e) {
-//            e.printStackTrace();
-//        }
-//
-//        finalIP = detectedIP.toString();
-//        finalIP = finalIP.substring(finalIP.lastIndexOf("/") + 1);
-//
-//        return finalIP;
 
     }
 }
